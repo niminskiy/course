367352674bbdb132ce63b0c5c79bf130653e1b32
niminskiy <s.niminskiy@gmail.com>
2020-11-25 17:45:31 +0300

    Условия

--- js/script.js
+++ js/script.js
@@ -1,29 +1,6 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/*const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
       count: numberOfFilms,
@@ -40,4 +17,17 @@ const a = prompt('Один из последних просмотренных ф
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+console.log(personalMovieDB);*/
+
+
+
+/*const num = 50;
+
+if (num < 50) {
+  console.log('Error!')
+} else if (num > 50) {
+  console.log('Error!')
+} else {
+  console.log('Ok!!!')
+}*/
+
